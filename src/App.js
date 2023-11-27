024b7240c4ace8f84130226e40d15c27a1c603a4
Chien Wei Chou <chien.w.chou@gmail.com>
2023-11-26 16:12:59 -0800

    add players

--- src/App.js
+++ src/App.js
@@ -6,61 +6,76 @@ import { useState } from 'react';
 const grids = [
   {
     id: 0,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 1,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 2,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 3,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 4,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 5,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 6,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 7,
-    value: false
+    value: false,
+    assigned: null
   },
   {
     id: 8,
-    value: false
+    value: false,
+    assigned: null
   }
 ];
 
+const player1 = '1';
+const player2 = '2';
+
 function App() {
   const [newGrid, setnewGrid] = useState(grids);
+  const [currentPlayer, setcurrentPlayer] = useState(player1);
 
   function setEachGrid(id, val) {
     const newGrid = grids.map(grid => {
       if (grid.id === id) {
         grid.value = val;
+        grid.assigned = currentPlayer;
       }
 
       return grid;
     });
 
     setnewGrid(newGrid);
+    setcurrentPlayer(currentPlayer === player1 ? player2 : player1);
   }
 
   function displayGrid(grids) {
     return grids.map(grid => {
       return (
-        <Grid key={grid.id} id={grid.id} gridChecked={grid.value} setEachGrid={setEachGrid} />
+        <Grid key={grid.id} id={grid.id} gridChecked={grid.value} playerAssigned={grid.assigned} setEachGrid={setEachGrid} />
       )
     });
   }
--- src/components/grid.js
+++ src/components/grid.js
@@ -1,8 +1,13 @@
 import './grid.css';
 
 function Grid(props) {
+    let pickedClass = '';
+    if (props.playerAssigned) {
+        pickedClass = props.playerAssigned === "1" ? " player-1-picked" : " player-2-picked"
+    }
+
     return (
-        <div id={'grid-' + props.id} className="col" onClick={() => props.setEachGrid(props.id, true)}>
+        <div id={'grid-' + props.id} className={"col" + (pickedClass)} onClick={() => props.setEachGrid(props.id, true)}>
             {props.gridChecked ? 'yes' : 'no'}
         </div>
     );
